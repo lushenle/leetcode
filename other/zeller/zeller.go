4c24b9d6c448994a0937817f6b48210c137bfad8
lushenle <lushenle@gmail.com>
2022-01-13 16:14:46 +0800

    add zeeler algo

---
+++ other/zeller/zeller.go
@@ -0,0 +1,55 @@
+// https://zh.wikipedia.org/wiki/%E8%94%A1%E5%8B%92%E5%85%AC%E5%BC%8F
+
+package zeller
+
+import (
+	"math"
+)
+
+func zeller(year, month, day int) float64 {
+	if month == 1 {
+		month = 13
+		year = year - 1
+	}
+
+	if month == 2 {
+		month = 14
+		year = year - 1
+	}
+
+	c := year / 100
+	year = year % 100
+	w := math.Ceil(float64((year + (year / 4) + (c / 4) - 2*c + (26 * (month + 1) / 10) + day + 700 - 1) % 7))
+
+	return w
+}
+
+//func main() {
+//	weekday := make(map[int]string)
+//
+//	weekday = map[int]string{
+//		0: "星期日",
+//		1: "星期一",
+//		2: "星期二",
+//		3: "星期三",
+//		4: "星期四",
+//		5: "星期五",
+//		6: "星期六",
+//	}
+//
+//	var (
+//		year  int
+//		month int
+//		day   int
+//	)
+//
+//	fmt.Println("enter year: ")
+//	fmt.Scan(&year)
+//	fmt.Println("enter month: ")
+//	fmt.Scan(&month)
+//	fmt.Println("enter day: ")
+//	fmt.Scan(&day)
+//
+//	w := Zeller(year, month, day)
+//	fmt.Println(weekday[int(w)])
+//}
---
+++ other/zeller/zeller_test.go
@@ -0,0 +1,43 @@
+package zeller
+
+import "testing"
+
+type args struct {
+	year  int
+	month int
+	day   int
+}
+
+func Test_zeller(t *testing.T) {
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "test01",
+			args: args{
+				year:  2022,
+				month: 1,
+				day:   13,
+			},
+			want: 4,
+		},
+		{
+			name: "test02",
+			args: args{
+				year:  1992,
+				month: 7,
+				day:   21,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := zeller(tt.args.year, tt.args.month, tt.args.day); got != tt.want {
+				t.Errorf("Zeller() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
