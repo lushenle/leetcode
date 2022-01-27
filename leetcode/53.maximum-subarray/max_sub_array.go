1126e16f5c35007f022b47798b9bf71144980a53
lushenle <lushenle@gmail.com>
2022-01-27 14:10:36 +0800

    add 53

---
+++ leetcode/53.maximum-subarray/max_sub_array.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp, res := make([]int, len(nums)), nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+		res = max(res, dp[i])
+	}
+	return res
+}
---
+++ leetcode/53.maximum-subarray/max_sub_array_test.go
@@ -0,0 +1,67 @@
+package leetcode
+
+import "testing"
+
+func Test_max(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test01",
+			args: args{a: 1, b: 2},
+			want: 2,
+		},
+		{
+			name: "test01",
+			args: args{a: -1, b: -2},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := max(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("max() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test01",
+			args: args{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}},
+			want: 6,
+		},
+		{
+			name: "test02",
+			args: args{nums: []int{1}},
+			want: 1,
+		},
+		{
+			name: "test03",
+			args: args{nums: []int{5, 4, -1, 7, 8}},
+			want: 23,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
