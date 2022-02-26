45063c20fe8fc67751073bd85bb82ebcf0544458
lushenle <lushenle@gmail.com>
2022-02-26 18:04:23 +0800

    add 46

---
+++ leetcode/46.permutations/permutations_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPermutations(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "testPermutations",
+			args: args{nums: []int{1, 2, 3}},
+			want: [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permute(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Permutations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
