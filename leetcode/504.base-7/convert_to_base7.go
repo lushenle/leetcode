c5387e65e0e80fc1fe19743e394f224965ad95ea
lushenle <lushenle@gmail.com>
2022-03-07 10:43:02 +0800

    add another solution for 504

--- leetcode/504.base-7/convert_to_base7.go
+++ leetcode/504.base-7/convert_to_base7.go
@@ -5,3 +5,29 @@ import "strconv"
 func convertToBase7(num int) string {
 	return strconv.FormatInt(int64(num), 7)
 }
+
+func abs(a int) int {
+	b := a >> 63
+	return (a ^ b) - b
+}
+
+func convertToBase71(num int) string {
+	if num == 0 {
+		return "0"
+	}
+
+	flag := num < 0
+	num = abs(num)
+	var s string
+
+	for num > 0 {
+		s = strconv.Itoa(num%7) + s
+		num /= 7
+	}
+
+	if flag {
+		s = "-" + s
+	}
+
+	return s
+}
--- leetcode/504.base-7/convert_to_base7_test.go
+++ leetcode/504.base-7/convert_to_base7_test.go
@@ -29,4 +29,12 @@ func Test_convertToBase7(t *testing.T) {
 			}
 		})
 	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertToBase71(tt.args.num); got != tt.want {
+				t.Errorf("convertToBase7() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
