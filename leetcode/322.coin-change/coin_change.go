74025553fe9a1ec04c0ce1d85db12540783166e8
lushenle <lushenle@gmail.com>
2022-04-11 18:54:51 +0800

    add 322

---
+++ leetcode/322.coin-change/coin_change.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+		for _, c := range coins {
+			if c <= i && dp[i] > dp[i-c]+1 {
+				dp[i] = dp[i-c] + 1
+			}
+		}
+	}
+
+	if dp[amount] > amount {
+		return -1
+	}
+
+	return dp[amount]
+}
